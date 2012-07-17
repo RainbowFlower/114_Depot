6ce66c09bd3a468bdb3f8058a5c0a9a5e22d129b
Lincaizhao <lincaizh@gmail.com>
2012-07-17 17:31:25 +0800

    Simple Ajax
    move the cart into side bar, accomplish ajax add of cart and highlight
    of new added cart lineitem

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,7 +46,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart, :notice => 'Line item was successfully created.') }
+        format.html { redirect_to(store_url) }
+		format.js	{ @current_item = @line_item}
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,7 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
+  
+  def total_items
+	line_items.sum(:quantity)
+  end
 end
