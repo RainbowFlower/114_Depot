49114e40096515885021098a4078df4430d3dcc6
Yangliang Li <yanglianglee@gmail.com>
2012-07-17 14:00:35 +0800

    Project sources first commit. Done: products presentation and shopping cart.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepotB::Application.config.secret_token = 'a11366101d634567c58d3f9a68dc9d0cf6b18754bd57c107377bd599ffb3a0bac4859255681bb03c52fde5b7369c566e3e29841c03c057defa67ebf9d3da932a'
