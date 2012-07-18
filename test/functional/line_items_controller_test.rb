d9d8b8eeb70ac78679e8d46b44a07bf61c248725
Lincaizhao <lincaizh@gmail.com>
2012-07-18 16:05:35 +0800

    Ajax Cart completed

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -18,10 +18,10 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, :line_item => @line_item.attributes
+      post :create, :product_id => products(:ruby).id
     end
 
-    assert_redirected_to line_item_path(assigns(:line_item))
+    assert_redirected_to store_path
   end
 
   test "should show line_item" do
