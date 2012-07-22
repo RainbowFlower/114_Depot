c012ba9cb3e1dd161542166dbf97a653eee50b60
sezina <sezinamd@gmail.com>
2012-07-22 23:04:15 +0800

    Add Atom Feeds.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ DepotB::Application.routes.draw do
 
   get "store/index"
 
-  resources :products
+  resources :products do
+    get :who_bought, :on => :member
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
