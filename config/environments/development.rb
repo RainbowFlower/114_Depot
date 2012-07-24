43c3a8defb8d5dd458eb7aee76c08b2ef177c943
HAJALG <761792105@qq.com>
2012-07-24 11:40:28 +0800

    1. Rails mailer settings. 2. Sending email to customer automatically while placing or shipping an order. 3. Functional and integration tests.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,15 @@
 DepotB::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :domain         => "domain.of.sender.net",
+    :authentication => "plain",
+    :user_name      => "dave",
+    :password       => "secret",
+    :enable_starttls_auto => true
+  }
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
