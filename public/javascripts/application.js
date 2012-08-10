3d68f2830331dfb6d96760ac21542981e62cfc93
Lincaizhao <lincaizh@gmail.com>
2012-08-10 09:19:43 +0800

    new look

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$j(document).ready(function(){
+	$j(".carousel").carousel({interval: 4000});
+	
+	$j(".carts").hover(
+		function() {
+			$j(".cart-content").show("fast");
+		},
+		function() {
+			$j(".cart-content").hide("fast");
+		}
+	);
+});
