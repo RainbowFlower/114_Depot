e07bf2cf43ea1a284a4fa67c9248083d75081bbc
Yangliang Li <yanglianglee@gmail.com>
2012-08-14 01:26:56 +0800

    modify product/show page and comments view.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.create(params[:comment])
-	@comment.user_id = session[:user_id]
-	@comment.save
-	redirect_to product_path(@product)
+		@comment.user_id = session[:user_id]
+		@comment.save
+		redirect_to product_path(@product)
   end
   
   def destroy
