d2cc8bc0ac9991cd3eefa58ca21a8b7012f64f60
Lincaizhao <lincaizh@gmail.com>
2012-08-15 10:00:00 +0800

    ajax add to cart in product show page

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,12 +9,13 @@ class Cart < ActiveRecord::Base
     line_items.to_a.sum{ |item| item.total_price}
   end
 
-  def add_product(product_id)
+  def add_product(product_id, quantity)
     current_item = line_items.where(:product_id => product_id).first
     if current_item
-      current_item.quantity += 1
+      current_item.quantity += quantity.to_i
     else
       current_item = LineItem.new(:product_id=>product_id)
+	  current_item.quantity = quantity.to_i
       line_items << current_item
     end
     current_item
