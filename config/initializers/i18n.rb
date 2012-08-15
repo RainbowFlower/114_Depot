aa85155c84723d38c1d760b3945cd0200bafd688
yangyu1 <1297734005@qq.com>
2012-08-15 11:42:57 +0800

    admin page i18n and remove language 'es'

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,6 +3,5 @@ I18n.default_locale = :en
 
 LANGUAGES = [
   ['English',                  'en'],
-  ['中文',                  'ch'],
-  ["Espa&ntilde;ol".html_safe, 'es']
-]
+  ['中文',                  'ch']
+  ]
